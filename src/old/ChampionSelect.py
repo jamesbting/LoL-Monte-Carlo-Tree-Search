57dcf9ac4531a2bf071a885d124aba2919a50280
James Ting <tingjames2000@outlook.com>
2021-02-26 16:31:46 -0800

    moved to old design

--- src/old/ChampionSelect.py
+++ src/old/ChampionSelect.py
@@ -1,6 +1,5 @@
 import json
 from random import random as random
-from player import player
 #states should be a tuple of tuples to be hashable eg. ((1,2,3,4,5),(6,7,8,9,10))
 class ChampionSelect:
     def __init__(self):
@@ -8,9 +7,9 @@ class ChampionSelect:
             self.validChampions = json.loads(f.read)
         f.close()
         self.selectedChampions = set()
-        self.current_player = None
+        self.curr_player = None
         self.next_player = 1 if random() > 0.5 else 0
-        self.players = [redteamplayer, redteamplayer]
+        self.players = []
 
     def current_player(self, state):
         blue_team = state[0]
