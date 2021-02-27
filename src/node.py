23d5bd819e55ecb2b994ca562779cc86dd3edbeb
James Ting <tingjames2000@outlook.com>
2021-02-26 17:01:07 -0800

    wrote node class

---
+++ src/node.py
@@ -0,0 +1,19 @@
+from math import pow, sqrt, log
+class Node(object):
+    def __init__(self, parent = None, state = None, possible_actions = None):
+        self.q = 0
+        self.n = 0
+        self.children = []
+        self.parent = parent
+        self.state = state
+        self.possible_actions = possible_actions
+
+    def expand(self, action):
+        new_child = Node(parent=self, state=self.state)
+        self.possible_actions.remove(action)
+        self.children.append(new_child)
+        return new_child
+
+    def bestChild(self, exploration_term = pow(2, -0.5)):
+        s = sorted(self.children, key = lambda c: c.q / c.n + (exploration_term * sqrt(2 * log(self.n)/c.n)))
+        return s[-1]
