1b504cd065d7d7911c4aca13a5a9956964f303a7
James Ting <tingjames2000@outlook.com>
2021-03-17 20:51:00 -0700

    added ability to count nodes

--- src/main.py
+++ src/main.py
@@ -1,37 +1,18 @@
 import json
 import time
 import psutil
-from filter import clean_data as filter
+import queue
 from monte_carlo import UCTSearch as UCT
 
 config = {
-    'filter': {
-        'enabled': False,
-        'desiredColumns': [
-            'teams.0.win',
-            'participants.0.championId',
-            'participants.1.championId',
-            'participants.2.championId',
-            'participants.3.championId',
-            'participants.4.championId',
-            'participants.5.championId',
-            'participants.6.championId',
-            'participants.7.championId',
-            'participants.8.championId',
-            'participants.9.championId', ],
-        'fileName': "../../../data/post-cleaning-dataset.csv",
-        'outputFileName': "data/filtered-dataset.csv"
-    },
     'champions': {
-        'fileName': "data/champions-cleaned.json"
+        'fileName': "../../data/champions-cleaned.json"
     },
     'iterations': 1000
 }
 
 
 def main():
-    if config['filter']['enabled']:
-        filter(config['filter']["fileName"], config['filter']["outputFileName"], config['filter']["desiredColumns"])
     champion_pool = json.load(open(config['champions']['fileName'], 'r'))
     initial_state = generate_initial_state(['121', '24', '18'], ['11', '26'], champion_pool)
     start_time = time.time()
@@ -41,9 +22,21 @@ def main():
 
 def show_results(result, time, memory_usage):
     print('Result:', result)
-    print('Took ',time , ' seconds to run')
-    print('Peak memory usage was: ', memory_usage/1000000, 'megabytes')
+    node_count = count_nodes(result.parent)
+    print('Monte Carlo Tree Search created', node_count, 'tree nodes.')
+    print('Took ',time , ' seconds to run.')
+    print('Peak memory usage was: ', memory_usage/1000000, 'megabytes.')
 
+def count_nodes(root):
+    q = queue.Queue()
+    count = 0
+    q.put(root)
+    while not q.empty():
+        curr = q.get()
+        count += 1
+        for child in curr.children:
+            q.put(child)
+    return count
 
 def generate_initial_state(blue_team, red_team, champion_pool):
     arr = [0] * len(champion_pool.keys())
